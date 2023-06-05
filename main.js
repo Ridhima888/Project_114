2a0b6e4dd648e1a0c456e4a3ca68c9388e7ca947
Ridhima888 <85024192+Ridhima888@users.noreply.github.com>
2023-06-05 20:28:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.center();
+    }
+function draw(){
+    
+}
+function take_snapshot(){
+    save('student_name.png');
+}
